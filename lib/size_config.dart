26491716ae397d288a7e5874313133e2458e64be
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-09-30 15:02:15 +0600

    Added size_config.dart file

---
+++ lib/size_config.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static late double defaultSize;
+  static late Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
+}
+
+// Get the proportionate height as per screen size
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+  // 896 is the layout height that designer use
+  // or you can say iPhone 11
+  // I'm using 800 for realme 7i
+  return (inputHeight / 800.0) * screenHeight;
+}
+
+// Get the proportionate width as per screen size
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+  // 414 is the layout width that designer use
+  // I'm using 360 for realme 7i
+  return (inputWidth / 360.0) * screenWidth;
+}
